584d0602d8de475227517a9ec12e8ab42c890cf7
luigimicco <luigi.micco@gmail.com>
2021-12-12 14:14:17 +0100

    code base

--- lib/button_widget.dart
+++ lib/button_widget.dart
@@ -1,91 +1,70 @@
 import 'package:flutter/material.dart';
 
-class ButtonWidget extends StatelessWidget{
+class ButtonWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       body: Center(
-
-        child: ElevatedButton(
-          style:  ElevatedButton.styleFrom(
-            onPrimary: Colors.green,
-            primary: Colors.yellow,
-            minimumSize: Size(88, 36),
-            padding: EdgeInsets.symmetric(horizontal: 16),
-            shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(Radius.circular(20)),
+        child: Column(
+          children: <Widget>[
+            ElevatedButton(
+              style: buttonStyle,
+              onPressed: null,
+              child: Text('Raised Button'),
             ),
-            elevation: 20.0,
-          ),
-          onPressed: () { },
-          child: Text('Raised Button'),
-        )
-
+            MaterialButton(
+              minWidth: 250.0,
+              onPressed: null,
+              colorBrightness: Brightness.dark,
+              color: Colors.deepPurpleAccent,
+              elevation: 20.0,
+              splashColor: Colors.green,
+              //highlightColor: Colors.red,
+              highlightElevation: 1.0,
+              child: Text("Material Button"),
+            ),
+            TextButton(
+              onPressed: null,
+              child: Text("Text Button"),
+              style: buttonStyle,
+            ),
+            OutlinedButton(
+              onPressed: () {},
+              child: Text("Outlined Button"),
+              style: buttonStyle,
+            ),
+            IconButton(
+                color: Colors.purple,
+                splashColor: Colors.yellow,
+                // highlightColor: Colors.red,
+                icon: Icon(
+                  Icons.build,
+                  size: 40.0,
+                ),
+                onPressed: () {}),
+          ],
+        ),
       ),
     );
   }
 
+  ButtonStyle buttonStyle = ElevatedButton.styleFrom(
+    onPrimary: Colors.green,
+    primary: Colors.yellow,
+    minimumSize: Size(88, 36),
+    padding: EdgeInsets.symmetric(horizontal: 16),
+    shape: const RoundedRectangleBorder(
+      borderRadius: BorderRadius.all(Radius.circular(20)),
+    ),
+    elevation: 20.0,
+  );
 }
 
 /*
-* TODO MaterialButton
-*
-* MaterialButton(
-          minWidth: 250.0,
-          onPressed: (){},
-          colorBrightness: Brightness.dark,
-          color: Colors.deepPurpleAccent,
-          elevation: 20.0,
-          splashColor: Colors.green,
-          //highlightColor: Colors.red,
-          highlightElevation: 1.0,
-          child: Text("Raised Button"),
-        ),
-
-* TODO FlatButton
-*
-* FlatButton(
-          onPressed: (){},
-          colorBrightness: Brightness.dark,
-          color: Colors.deepPurpleAccent,
-          splashColor: Colors.green,
-          highlightColor: Colors.red,
-          child: Text("Raised Button"),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.horizontal(left: Radius.circular(20.0), right: Radius.circular(1.0))
-          ),
-        ),
 *
 
 * TODO OutlineButton
-*
-* OutlineButton(
-          onPressed: (){},
-          borderSide: BorderSide(
-            width: 5.0,
-            color: Colors.deepPurpleAccent
-          ),
-          color: Colors.deepPurpleAccent,
-          highlightedBorderColor: Colors.purple,
-          splashColor: Colors.green,
-          //highlightColor: Colors.red,
-          child: Text("Raised Button"),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.vertical(top: Radius.circular(20.0), bottom: Radius.circular(1.0))
-          ),
-        ),
-*
-
-
-* IconButton(
-            color: Colors.purple,
-            splashColor: Colors.yellow,
-           // highlightColor: Colors.red,
-            icon: Icon(Icons.build, size: 40.0,),
-            onPressed: (){})
-      ),
-
 
 * TODO Simple Circular Button using FloatingActionButton (Mini and Normal Size)
 *
--- lib/text_widget.dart
+++ lib/text_widget.dart
@@ -6,29 +6,29 @@ class TextWidget extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(),
       body: Center(
-        child: Text(
-          "Hello Flutter",
+        child: Column(
+          children: <Widget>[
+            Text(
+              "Hello Flutter",
+            ),
+            Text(
+              "Hello Flutter It is Awesome WOW",
+              textAlign: TextAlign.right,
+              textDirection: TextDirection.ltr,
+              overflow: TextOverflow.ellipsis,
+              maxLines: 2,
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w200,
+                  letterSpacing: 2.0,
+                  wordSpacing: 10.0,
+                  decoration: TextDecoration.overline,
+                  decorationStyle: TextDecorationStyle.wavy),
+            ),
+          ],
         ),
       ),
     );
   }
 }
-
-/*
-* Text(
-          "Hello Flutter It is Awesome WOW",
-          textAlign: TextAlign.right,
-          textDirection: TextDirection.ltr,
-          overflow: TextOverflow.ellipsis,
-          maxLines: 2,
-          style: TextStyle(
-              color: Colors.black,
-              fontSize: 50.0,
-              fontWeight: FontWeight.w200,
-              letterSpacing: 2.0,
-              wordSpacing: 40.0,
-              decoration: TextDecoration.overline,
-              decorationStyle: TextDecorationStyle.wavy),
-        ),
-*
-* */
